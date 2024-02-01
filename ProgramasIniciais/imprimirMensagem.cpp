7f9659b5eddfcf9870b7decf6bbf292e6ff83f38
Lucasvgb <lucasvgb@id.uff.br>
2024-01-31 21:22:08 -0300

    FuncaoDeImpressao

---
+++ ProgramasIniciais/imprimirMensagem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void ImprimirMensagem(){
+
+  std::cout <<"Essa eh uma mensagem na tela" << std::endl; // Adiciona '\n' no final
+
+}
+
+int main(){
+
+  ImprimirMensagem(); // Chama a função
+
+  return 0;
+}
