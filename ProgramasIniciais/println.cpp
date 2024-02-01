acf8fde230f0bc522784d89fa080255829d25463
Lucasvgb <lucasvgb@id.uff.br>
2024-01-31 21:03:07 -0300

    Boas praticas de programacao

--- ProgramasIniciais/println.cpp
+++ ProgramasIniciais/println.cpp
@@ -1,5 +1,9 @@
 #include <iostream> //Permite que o programa gere saída de DADOS na tela
 
+/*
+Cada programa deve iniciar com um comentário que descreve o propósito do programa, autor, data e hora. (Não mostramos o autor,
+data e hora nos programas deste livro porque essas informações seriam redundantes.)
+*/
 int main(){
 
   std::cout << "Bem vindo ao C++";
