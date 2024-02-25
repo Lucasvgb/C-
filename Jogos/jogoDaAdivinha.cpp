3b0517c8d2810e46b4e5deaf6a106eeabf28d896
Lucasvgb <lucasvgb@id.uff.br>
2024-02-25 18:18:33 -0300

    Adicionando um sistema de pontuação.

--- Jogos/jogoDaAdivinha.cpp
+++ Jogos/jogoDaAdivinha.cpp
@@ -10,17 +10,24 @@ int main(){
   cout << "****************************" << endl;
 
   const int NUMERO_SECRETO  = 42; //Por padrão as constantes ficam em CAPS LOCK.
+
   bool nao_acertou = true;
   int tentativas = 0;
   
+  double pontos = 1000.0;
+
   while(nao_acertou){
 
     tentativas += 1;
+
     int chute;
     cout <<"Tentativa: " << tentativas << endl;
     cout <<"Qual seu chute ? ";
     cin >> chute;
     cout <<"O valor do seu chute foi: " << chute << endl;
+
+    double pontos_perdidos = abs(chute - NUMERO_SECRETO) / 2.0; //usando o valor absoluto.
+    pontos -= pontos_perdidos;
 
     bool acertou = chute == NUMERO_SECRETO;
     bool maior = chute > NUMERO_SECRETO;
@@ -39,6 +46,7 @@ int main(){
 
   cout <<"Fim de jogo" << endl;
   cout <<"Voce acertou o numero em: " << tentativas << " tentativas." << endl;
+  cout <<"Sua pontuacao foi de: " << pontos <<" pontos." << endl;
 
   return 0;
 }
