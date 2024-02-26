135c609ab2fdf260b072a0948cd8ff728a8f5c78
Lucasvgb <lucasvgb@id.uff.br>
2024-02-26 14:45:29 -0300

    alterando as pastas

---
+++ Jogos/jogoDaForca/jogoDaForca.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  //Escolhendo uma palavra
+  string palavra_secreta = "BANANA";
+  cout << "A palavra eh: " << palavra_secreta << endl;
+  
+
+
+  return 0;
+}
